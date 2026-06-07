1323a6acbba45c5358f11342dec3d47cbdef884c
Lucio junior <luciojunin99@hotmail.com>
2026-06-07 16:23:56 -0300

    feat: refactor error handling in ClearInactiveTableParticipants command

--- app/Console/Commands/ClearInactiveTableParticipants.php
+++ app/Console/Commands/ClearInactiveTableParticipants.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Order;
 use App\Models\TableParticipant;
+use Illuminate\Support\Facades\Log;
 
 class ClearInactiveTableParticipants extends Command
 {
@@ -27,23 +28,33 @@ class ClearInactiveTableParticipants extends Command
      */
     public function handle()
     {
-        $Orders = Order::where('payment_status', Order::PAYMENT_STATUS_PAID)
-            ->where('updated_at', '<=', now()->subMinutes(5))
-            ->get();
+        try {
+            $Orders = Order::where('payment_status', Order::PAYMENT_STATUS_PAID)
+                ->where('updated_at', '<=', now()->subMinutes(5))
+                ->get();
 
-        foreach ($Orders as $order) {
-            $Participant = TableParticipant::where('id', $order->participant_id)
-                ->delete();
-        }
-
-        $Participants = TableParticipant::where('created_at', '<=', now()->subMinutes(60))->get();
-
-        foreach ($Participants as $participant) {
-            $Order = Order::where('participant_id', $participant->participant_id)->first();
-
-            if (!$Order) {
-                $participant->delete();
+            foreach ($Orders as $order) {
+                $Participant = TableParticipant::where('id', $order->participant_id)
+                    ->delete();
             }
+
+            $Participants = TableParticipant::where('created_at', '<=', now()->subMinutes(60))->get();
+
+            foreach ($Participants as $participant) {
+                $Order = Order::where('participant_id', $participant->participant_id)->first();
+
+                if (!$Order) {
+                    $participant->delete();
+                }
+            }
+        } catch (\Exception $e) {
+
+            Log::error('Erro ao limpar participantes inativos da mesa: ', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ]);
         }
     }
 }
